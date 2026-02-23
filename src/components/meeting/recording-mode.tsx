13ab24fc0030d013cc0104eec8837470c223bdeb
d.takamiya <d.miyataka@gmail.com>
2026-02-23 10:10:55 +0900

    style: Prettier自動フォーマット適用

--- src/components/meeting/recording-mode.tsx
+++ src/components/meeting/recording-mode.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect,useMemo, useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import { toast } from "sonner";
 
 import { Button } from "@/components/ui/button";
